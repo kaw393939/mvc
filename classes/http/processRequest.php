6cd7e757934384c6cbbfbf8687524cf380fef93d
kaw393939 <keith@webizly.com>
2017-11-27 22:06:33 -0500

    added page not found

--- classes/http/processRequest.php
+++ classes/http/processRequest.php
@@ -52,16 +52,20 @@ class processRequest
 
         //this gets the routes objects, you need to add routes to add pages and follow the template of the route specified
         $routes = \routes::getRoutes();
-
+        $foundRoute = NULL;
         //this figures out which route matches the page being requested in the URL and returns it so that the controller and method can be called
         foreach ($routes as $route) {
 
             if ($route->page == $page && $route->http_method == $request_method && $route->action == $action) {
-                return $route;
+                $foundRoute = $route;
+                break;
             }
-
-
+        }
+        if(is_null($foundRoute)) {
+            controller::getTemplate('notfound');
+            exit;
+        } else {
+            return $foundRoute;
         }
     }
-
 }
